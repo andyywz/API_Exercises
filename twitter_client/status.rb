a591d60347d54bf8e68f469a20f24b5e8f24d79a
App Academy Student <student@appacademy.io>
2013-07-08 16:13:47 -0400

    Extends Status class.

--- twitter_client/status.rb
+++ twitter_client/status.rb
@@ -1,12 +1,17 @@
 class Status
   attr_accessor :text, :user
 
-  def initialize(author, message)
-    @user = author
-    @text = message
+  def initialize(hash)
+    @user = hash["user"]["name"]
+    @text = hash["text"]
+    @hashtags = hash["entities"]["hashtags"]
+    @mentions = hash["entities"]["user_mentions"]
   end
 
   def self.parse(json)
-    Status.new(json["user"]["name"], json["text"])
+    Status.new(json)
   end
+
+
+
 end
