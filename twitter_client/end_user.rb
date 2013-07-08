48e176f12e23ec9db71dcc2b0e0d8554ad47aeb1
App Academy Student <student@appacademy.io>
2013-07-08 15:41:16 -0400

    Finishes main project requirements

--- twitter_client/end_user.rb
+++ twitter_client/end_user.rb
@@ -1,5 +1,36 @@
 require_relative 'user'
 
 class EndUser < User
+  def self.set_user_name(user_name)
+    @@current_user = EndUser.new(user_name)
+  end
 
+  def self.me
+    @@current_user
+  end
+
+  def post_status(status_text)
+    request = Addressable::URI.new(
+      :scheme => "http",
+      :host => "api.twitter.com",
+      :path => "1.1/statuses/update.json",
+      :query_values => {"status" => status_text}
+      ).to_s
+
+    TwitterSession.access_token.post(request)
+  end
+
+  def direct_message(other_user, text)
+    request = Addressable::URI.new(
+      :scheme => "http",
+      :host => "api.twitter.com",
+      :path => "1.1/direct_messages/new.json",
+      :query_values => {
+        "screen_name" => other_user,
+        "text" => text
+      }
+      ).to_s
+
+    TwitterSession.access_token.post(request)
+  end
 end
